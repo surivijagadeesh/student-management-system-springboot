505a5fb29ab2114e066f6f6824a195ba7664826e
surivijagadeesh <surivijagadeesh@gmail.com>
2024-09-08 13:07:17 +0530

    Entity changed

--- src/main/java/com/sj/sms/entity/Student.java
+++ src/main/java/com/sj/sms/entity/Student.java
@@ -31,28 +31,4 @@ public class Student {
 		this.lastName = lastName;
 		this.email = email;
 	}
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
 }
